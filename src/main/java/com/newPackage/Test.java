ff0366042dcc7d4bd2ba4fcf34ca2f40f259a606
IST <viswarupaachari88@gmail.com>
2020-09-09 16:13:19 +0530

    Code Coverage is Done.....!

--- src/main/java/com/newPackage/Test.java
+++ src/main/java/com/newPackage/Test.java
@@ -4,11 +4,8 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class Test {
